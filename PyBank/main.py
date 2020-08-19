c36d3d32750f5e707a81cc4b7ee61403909c04ef
amberreynolds <reynolds.j.amber@gmail.com>
2020-08-19 19:59:21 -0400

    Complete the incomplete

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,33 +9,65 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvfile)
 
+#net amount of profit and loss
+Profit = []
+months = []
 
-    # for row in csvreader:
-    #     print("CSV row: {0}".format(row))
-    #print(csvreader)
+#read through each row of data after header
+for rows in csvreader:
+    Profit.append(int(rows[1]))
+    months.append(rows[0])
 
-    #csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
+#revenue change
+revenue_change = []
 
-    #for row in csvreader:
-        #print(row)
+for x in range(1, len(Profit)):
+    revenue_change.append((int(Profit[x]) - int(Profit[x-1])))
+    
+# avg revenue change
+revenue_average = sum(revenue_change) / len(revenue_change)
+    
+#total length of months
+total_months = len(months)
 
-    print(csvreader.line_num)
+# greatest increase in revenue
+greatest_increase = max(revenue_change)
+# greatest decrease in revenue
+greatest_decrease = min(revenue_change)
 
 print("Financial Analysis")
 print("----------------------------")    
 
 #Total Numner of Months:
-#print("Total Months: " )
+print("Total Months: " + str(total_months))
 
 #Net Total Amount of "Profit/Losses"
-#print("Total: " + )
+print("Total: " + "$" + str(sum(Profit)))
 
 #Average of the changes in "Profit/Losses"
-# print("Average Change: " + )
+print("Average Change: " + "$" + str(revenue_average))
 
 #Greatest increase in profits (date and amount)
-# print("Greatest Increase in Profits: " + )
+print("Greatest Increase in Profits: " + str(months[revenue_change.index(max(revenue_change))+1]) + " " + "$" + str(greatest_increase))
 
 # Greatest decrease in losses (date and amount)
-# print("Greatest Decrease in Profits: " + )
+print("Greatest Decrease in Profits: " + str(months[revenue_change.index(min(revenue_change))+1]) + " " + "$" + str(greatest_decrease))
+
+# Print to text file
+file = open("output.txt","w")
+
+file.write("Financial Analysis" + "\n")
+
+file.write("...................................................................................." + "\n")
+
+file.write("total months: " + str(total_months) + "\n")
+
+file.write("Total: " + "$" + str(sum(P)) + "\n")
+
+file.write("Average change: " + "$" + str(revenue_average) + "\n")
+
+file.write("Greatest Increase in Profits: " + str(months[revenue_change.index(max(revenue_change))+1]) + " " + "$" + str(greatest_increase) + "\n")
+
+file.write("Greatest Decrease in Profits: " + str(months[revenue_change.index(min(revenue_change))+1]) + " " + "$" + str(greatest_decrease) + "\n")
+
+file.close()
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,3 +6,37 @@ csvpath = os.path.join("Resources/election_data.csv")
 with open(csvpath, newline="") as csvfile:
     csv_reader =csv.reader(csvfile, delimiter=",")
     csv_headers = next(csv_reader)
+
+# calculate the total number of votes
+total_votes = len(csvfile)
+total_votes
+
+# list of candidates who received votes
+candidates_count = csvfile["Candidate"].value_counts()
+
+# percentage of votes each candidate won
+percentage_votes = (candidates_count/total_votes)*100
+
+# announce the winner
+winner = candidates_count.idxmax()
+
+# print the results
+print("Election results")
+
+print("------------------------------------------------")
+
+print("Total votes: " + str(total_votes))
+
+print("------------------------------------------------")
+
+print("Khan:" + " " + str(round(percentage_votes[0],3)) + "%" + "("+str(candidates_count[0])+")")
+      
+print("Correy:" + " " + str(round(percentage_votes[1],3)) + "%" + "("+str(candidates_count[1])+")")
+      
+print("Li:" + " " + str(round(percentage_votes[2],3)) + "%" + "("+str(candidates_count[2])+")")
+      
+print("O'Tooley:" + " " + str(round(percentage_votes[3],3)) + "%" + "("+str(candidates_count[3])+")")
+
+print("----------------------------------------------------------------------------------------")
+      
+print("winner: " + winner)
